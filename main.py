cbc02d66d59888f953f03eeeb50594f9228c5b9e
Christian Hiricoiu <christian.hiricoiu@gmail.com>
2020-03-29 18:36:38 +0200

    timer added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from python_anticaptcha import AnticaptchaClient, ImageToTextTask
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 import tkinter
 import xml.etree.ElementTree as ET
@@ -21,10 +21,14 @@ class Application(object):
         self.passw_entree = tkinter.Entry(self.root, width=20)
         self.passw_entree.grid(row=1, column=1, columnspan=2)
 
-        tkinter.Button(self.root, text='Launch', command=combine_funcs(self.start_enroll, self.enroll)) \
+        tkinter.Button(self.root, text="Launch", command=combine_funcs(self.start_enroll, self.enroll)) \
             .grid(row=2, column=1)
-        tkinter.Button(self.root, text='Stop', command=self.stop_enroll) \
+        tkinter.Button(self.root, text="Stop", command=self.stop_enroll) \
             .grid(row=2, column=2)
+
+        tkinter.Label(self.root, text="Next enroll in :").grid(row=3, column=0)
+        self.timer_label = tkinter.Label(self.root, text="--:--:--", font=('Helvetica', 12), fg='red')
+        self.timer_label.grid(row=3, column=1)
 
         self.menuBar = tkinter.Menu(self.root)
 
@@ -37,14 +41,15 @@ class Application(object):
         self.menuBar.add_cascade(label="Help", menu=self.menuHelp)
         self.root.config(menu=self.menuBar)
 
-        # self.chromedriver_path = '/usr/lib/chromium-browser/libs/chromedriver'
+        #  self.chromedriver_path = '/usr/lib/chromium-browser/libs/chromedriver'
         self.chromedriver_path = 'C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe'
-        # self.captcha_filepath = '/home/pi/Pictures/captcha.jpeg'
+        #  self.captcha_filepath = '/home/pi/Pictures/captcha.jpeg'
         self.captcha_filepath = 'C:/Users/Foussy/Pictures/LordsWM_captchas/captcha.jpeg'
         self.character_page_url = 'https://www.lordswm.com/pl_info.php?id=5526781'
 
         self.enroll_state = False
-        self.timer = 1000
+        self.static_timer = 0
+        self.countdown = 0
         self.root.mainloop()
 
     def stop_enroll(self):
@@ -56,7 +61,6 @@ class Application(object):
         print('programm resume : {0}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
     def enroll(self):
-
         if self.enroll_state:
             chromepage = open_chromepage(self.chromedriver_path)
             try:
@@ -64,27 +68,33 @@ class Application(object):
                 login_homepage(chromepage, self.login_entree.get(), self.passw_entree.get())
                 region = player_region(chromepage, self.character_page_url)
                 prod_sites, machin_sites, mines_sites = load_locations(region)
-
-                url = find_location_to_work(chromepage, prod_sites)
-                if not url:
-                    url = find_location_to_work(chromepage, machin_sites)
-
-                    if not url:
-                        url = find_location_to_work(chromepage, mines_sites)
-
+                url_list = prod_sites + machin_sites + mines_sites
+                url = find_location_to_work(chromepage, url_list)
                 captcha_filepath = download_captcha(chromepage, url, self.captcha_filepath)
                 code = solve_captcha(captcha_filepath)
                 send_captcha(chromepage, code)
 
-                if check_enrollment(chromepage):
-                    self.timer = 3600 * 1000
+                check_enrollment(chromepage)
 
             except Exception as Error:
                 print(Error)
-                self.timer = 3600 * 1000
+
             finally:
+                self.static_timer = 3600
                 chromepage.quit()
-                self.root.after(self.timer, self.enroll)
+                self.update_timer(self.static_timer)
+                self.root.after(self.static_timer * 1000, self.enroll)
+
+    def update_timer(self, countdown=None):
+        if countdown is not None:
+            self.countdown = countdown
+
+        if self.countdown <= 0:
+            self.timer_label.configure(text="--:--:--")
+        else:
+            self.timer_label.configure(text=str(timedelta(seconds=self.countdown)))
+            self.countdown = self.countdown - 1
+            self.root.after(1000, self.update_timer)
 
     def parameters_window(self):
         print(self.chromedriver_path)
@@ -213,6 +223,7 @@ def combine_funcs(*funcs):
     def combined_func(*args, **kwargs):
         for f in funcs:
             f(*args, **kwargs)
+
     return combined_func
 
 
